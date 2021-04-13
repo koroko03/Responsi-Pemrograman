e6bb7c92620b5e68711d8172b12682f38579c2fe
koroko03 <korokonamae@gmail.com>
2021-04-13 19:56:29 +0700

    Mengubah isi koding

--- Karyawan.cs
+++ Karyawan.cs
@@ -6,33 +6,26 @@ using System.Threading.Tasks;
 
 namespace ResponsiPemrograman3805
 {
-    class Karyawan
+    public class Karyawan
     {
-        // Private variable
-        private string NIK;
-        private string Nama;
-        private int GajiBulanan;
+        private object a;
 
-        public Karyawan()
+        public string NIK { get; set; }
+        public string Nama { get; set; }
+        public int Gaji { get; set; }
+
+        public void tambah()
         {
-            Nama = "Paijo";
-            Nama = "Jono";
-            NIK = "19030123";
-            NIK = "19030124";
-            GajiBulanan = 3000000;
-            GajiBulanan = 2000000;
+            Console.WriteLine("No. NIK      Nama  Gaji Bulanan");
+            Console.WriteLine("==================================");
+            Console.WriteLine("1.  " + NIK + " " + Nama + " " +  Gaji);
         }
 
-       
-        // Method
-        public void inisialisasi()
+        public void tambah2()
         {
-            Console.WriteLine("No.  NIK   Nama  Gaji Bulanan {0}");
-            Console.WriteLine("{0}", NIK);
-            Console.WriteLine("{0}", Nama);
-            Console.WriteLine("{0}", GajiBulanan);
-
+            Console.WriteLine("2.  " + NIK + " " + Nama + "  " + Gaji);
         }
 
     }
+        
 }
--- Program.cs
+++ Program.cs
@@ -10,9 +10,42 @@ namespace ResponsiPemrograman3805
     {
         static void Main(string[] args)
         {
-            Karyawan kry = new Karyawan();
-            kry.inisialisasi();
-            
+            Karyawan kry1 = new Karyawan();
+            Karyawan kry2 = new Karyawan();
+
+            bool TambahGaji = true;
+
+            kry1.NIK = "19030123";
+            kry1.Nama = "Paijo";
+            kry1.Gaji = 3000000;
+
+            kry2.NIK = "19030124";
+            kry2.Nama = "Jono";
+            kry2.Gaji = 2000000;
+
+            kry1.tambah();
+            kry2.tambah2();
+
+            double a = 3000000.0;
+            double b = 0.1;
+            double c = 2000000.0;
+            double hasil1 = (a * b) + a;
+            double hasil2 = (c * b) + c;
+
+
+            if (TambahGaji == true)
+            {
+                Console.WriteLine("\nAlhamdulillah, Selamat anda mendapat kenaikan gaji\n");
+                Console.WriteLine("No. NIK      Nama  Gaji Bulanan(Rp)");
+                Console.WriteLine("==================================");
+                Console.WriteLine("1.  " + kry1.NIK + " " + kry1.Nama + " " + hasil1);
+                Console.WriteLine("2.  " + kry2.NIK + " " + kry2.Nama + "  " + hasil2);
+                Console.WriteLine("\n");
+            }
+            else
+                Console.WriteLine("\nMohon maaf belum ada kenaikan gaji\n");
+
+            Console.WriteLine("Press any key....");
             Console.ReadKey();
         }
     }
