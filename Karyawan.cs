60fd8178fe4539424699efe2372749fc3a175cb9
koroko03 <korokonamae@gmail.com>
2021-04-13 13:10:40 +0700

    Menambahkan Program

---
+++ Karyawan.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman3805
+{
+    class Karyawan
+    {
+        // Private variable
+        private string NIK;
+        private string Nama;
+        private int GajiBulanan;
+
+        public Karyawan()
+        {
+            Nama = "Paijo";
+            Nama = "Jono";
+            NIK = "19030123";
+            NIK = "19030124";
+            GajiBulanan = 3000000;
+            GajiBulanan = 2000000;
+        }
+
+       
+        // Method
+        public void inisialisasi()
+        {
+            Console.WriteLine("No.  NIK   Nama  Gaji Bulanan {0}");
+            Console.WriteLine("{0}", NIK);
+            Console.WriteLine("{0}", Nama);
+            Console.WriteLine("{0}", GajiBulanan);
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman3805
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan kry = new Karyawan();
+            kry.inisialisasi();
+            
+            Console.ReadKey();
+        }
+    }
+}
